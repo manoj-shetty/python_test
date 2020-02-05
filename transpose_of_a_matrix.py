8730e83a1a13c287f1c608fe3064add5d65d07ea
Manoj <manoj3121kumar@gmail.com>
2020-02-05 17:14:02 +0530

    Update transpose_of_a_matrix.py

--- transpose_of_a_matrix.py
+++ transpose_of_a_matrix.py
@@ -5,6 +5,5 @@ A = [ [1, 1, 1, 1],
 N = 4
 for i in range(N): 
     for j in range(i+1, N):
-        if i != j:
-            A[i][j], A[j][i] = A[j][i], A[i][j]
+        A[i][j], A[j][i] = A[j][i], A[i][j]
 print(A)
